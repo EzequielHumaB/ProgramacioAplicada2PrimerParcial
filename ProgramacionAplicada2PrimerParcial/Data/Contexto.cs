8359b598394175ca05e30f59d4cad0989729c602
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-02-05 13:28:41 -0400

    Agregando las carpetas Modelos y Controladores, y agregando la clase Contexto, con su respectivo ConnectionString

---
+++ ProgramacionAplicada2PrimerParcial/Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProgramacionAplicada2PrimerParcial.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlServer(@"Server=DESKTOP-B3FBQ4F;Database=ProgramacionAplicada2PrimerPacialDb;Trusted_Connection"));
+        }
+    }
+}
